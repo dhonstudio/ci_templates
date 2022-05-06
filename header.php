ba657afcd16f9c38dd682992569d93b3433063b8
dhonstudio <doon13@gmail.com>
2022-05-07 01:43:16 +0700

    Adding body id

--- header.php
+++ header.php
@@ -6,23 +6,22 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    
-    <meta name="keywords" content="<?= isset($meta['keywords']) ? $meta['keywords'] : ""?>">
+
+    <meta name="keywords" content="<?= isset($meta['keywords']) ? $meta['keywords'] : "" ?>">
     <meta name="author" content="<?= isset($meta['author']) ? $meta['author'] : "" ?>">
     <meta name="generator" content="<?= isset($meta['generator']) ? $meta['generator'] : "" ?>">
 
     <meta property="og:image" content="<?= isset($meta['ogimage']) ? $meta['ogimage'] : "https://dhonstudio.com/assets/img/ogimg.jpg" ?>">
-    <meta name="description" content="<?= isset($meta['description']) ? $meta['description'] : ""?>">
+    <meta name="description" content="<?= isset($meta['description']) ? $meta['description'] : "" ?>">
 
     <link rel="icon" type="image/x-icon" href="<?= isset($favicon) ? $favicon : "https://dhonstudio.com/assets/img/icon.ico" ?>">
     <title><?= isset($title) ? $title : "Home" ?></title>
 
-    <?= isset($font) ? implode($font) : ''?>
-    <?= isset($css) ? implode($css) : ''?>
-    <?= isset($js) ? implode($js) : ''?>
-    <?= isset($custom_style) ? implode($custom_style) : ''?>
+    <?= isset($font) ? implode($font) : '' ?>
+    <?= isset($css) ? implode($css) : '' ?>
+    <?= isset($js) ? implode($js) : '' ?>
+    <?= isset($custom_style) ? implode($custom_style) : '' ?>
 
 </head>
 
-<body class="<?= isset($body_class) ? $body_class : "" ?>" style="<?= isset($body_style) ? $body_style : "" ?>">
-    
+<body id="<?= isset($body_id) ? $body_id : "" ?>" class="<?= isset($body_class) ? $body_class : "" ?>" style="<?= isset($body_style) ? $body_style : "" ?>">
