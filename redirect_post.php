2e970effb9f4976662fd26e2843664b062ac74c4
dhonstudio <doon13@gmail.com>
2022-06-06 08:03:04 +0700

    fixed action in redirect post

--- redirect_post.php
+++ redirect_post.php
@@ -1,8 +1,8 @@
-<form id="redirectForm" action="<?= base_url($action) ?>" method="post">
-    <?php foreach ($posts as $key => $p):?>
-        <?php $count = $key+1?>
-        <input hidden name="<?= $p['post_name'.$count] ?>" value="<?= $p['post_value'.$count] ?>">
-    <?php endforeach?>
+<form id="redirectForm" action="<?= $action ?>" method="post">
+    <?php foreach ($posts as $key => $p) : ?>
+        <?php $count = $key + 1 ?>
+        <input hidden name="<?= $p['post_name' . $count] ?>" value="<?= $p['post_value' . $count] ?>">
+    <?php endforeach ?>
 </form>
 
 <script type="text/javascript">
